ec98c529cb9047829420dccad1d8e34366ca02a9
Guantz2023 <123930796+Guantz2023@users.noreply.github.com>
2024-10-02 21:29:53 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '5d5b9e19-1a73-415c-91f0-aa96d4b98777';
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
 const proxyIPs= ['bpb.yousef.isegaro.com'];
